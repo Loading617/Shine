1ecea2c22b90fa2a77e4ff5813c162d1f7e869f6
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-02-29 12:54:44 -0500

    Add files via upload

---
+++ main.java
@@ -0,0 +1,13 @@
+/* package whatever; //
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class 
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	}
+}
