f09724893c7365a7c8204162c964a8236745c297
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-09-16 10:37:34 -0400

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,9 @@
+p ublic class App {
+
+    public static void main(String[] args)throws Exception {
+        System.out.println("Hello World!");
+
+        MainFrame myFrame = new MainFrame();
+        myFrame.init();
+    }
+}
