6a62d862c200161220f5fd1032a4e2b544bf0196
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-10-02 15:30:42 -0400

    Add files via upload

---
+++ c++/j2mesavemenu.cpp
@@ -0,0 +1,12 @@
+#include "j2me save game menu"
+
+// Function to display the save game menu
+
+void displaySaveGameMenu() {
+    printf("\nSave Game Menu\n");
+    printf("1. Load Game\n");
+    printf("2. Save Game\n");
+    printf("3. Delete Game\n");
+    printf("4. Exit\n");
+    printf("Enter your choice: ");
+}
