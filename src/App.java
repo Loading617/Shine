2d54766ed08ecb751467f1667e4c9d297066f87e
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-11-23 20:36:47 -0500

    Delete src directory

--- src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-
-    public static void main(String[] args)throws Exception {
-        System.out.println("Hello World!");
-
-        MainFrame myFrame = new MainFrame();
-        myFrame.init();
-    }
-}
