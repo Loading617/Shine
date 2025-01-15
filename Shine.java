88ae26497fbb09838ebaa1aaf9c56c3ea8413c3b
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-01-14 21:05:24 -0500

    Update Shine.java

--- Shine.java
+++ Shine.java
@@ -1,18 +1,20 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class Shine extends Application {
     @Override
     public void start(Stage primaryStage) {
+        
         primaryStage.setTitle("Shine");
-
+        
+        primaryStage.initStyle(StageStyle.DECORATED);
+        
         primaryStage.setResizable(false);
-
-        primaryStage.initStyle(StageStyle.UTILITY);
-
-        primaryStage.setWidth(400);
-        primaryStage.setHeight(300);
         primaryStage.show();
     }
 
