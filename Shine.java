d4e0425ffb6c3183cf41061525b8fec5b7b36644
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-02-24 16:14:44 -0500

    Update Shine.java

--- Shine.java
+++ Shine.java
@@ -1,6 +1,5 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -8,32 +7,31 @@ import javafx.stage.Stage;
 public class Shine extends Application {
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Shine");
         
-        MenuBar menubar = new MenuBar();
-        
-        Menu midletMenu = new Menu("Midlet");
-        Menu toolsMenu = new Menu("Tools");
-        Menu viewMenu = new Menu("View");
-        
-        MenuItem newItem = new MenuItem("Load jar");
-        MenuItem newItem = new MenuItem("Recent");
-        MenuItem newItem = new MenuItem("Exit");
-        
-        exitItem.setOnAction(e -> primaryStage.close());
-        
-        fileMenu.getItems().addAll(newItem, openItem, saveItem, new SeparatorMenuItem(), exitItem);
-        
-        menuBar.getMenus().addAll(midletMenu, recentMenu, viewMenu);
-        
-        VBox root = new VBox(menubar);
-        Scene Scene = new Scene(root, 240, 320);
-        
-        Scene scene = new Scene(root, 240, 320);
-        primaryStage.setScene(scene);
-        
-        primaryStage.setResizable(false);
+        MenuBar menuBar = new MenuBar();
 
+        Menu fileMenu = new Menu("Midlet");
+        Menu editMenu = new Menu("Tools");
+        Menu helpMenu = new Menu("View");
+
+        MenuItem midletItem = new MenuItem("Load Jar/Jad File");
+        MenuItem recentItem = new MenuItem("Recent");
+        MenuItem recordItem = new MenuItem("Record");
+        MenuItem screenshotItem = new MenuItem("Screenshot");
+        MenuItem exitItem = new MenuItem("Exit");
+
+        exitItem.setOnAction(e -> primaryStage.close());
+
+        fileMenu.getItems().addAll(midletItem, recentItem, recordItem, screenshotItem, new SeparatorMenuItem(), exitItem);
+
+        menuBar.getMenus().addAll(fileMenu, editMenu, helpMenu);
+
+        VBox root = new VBox(menuBar);
+        Scene scene = new Scene(root, 240, 320);
+
+        primaryStage.setTitle("Shine");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
