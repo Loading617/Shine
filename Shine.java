32ef09e880b59acc831ab5eddd5db2b637f4fe12
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-03-04 14:31:22 -0500

    Update Shine.java

--- Shine.java
+++ Shine.java
@@ -1,6 +1,10 @@
 import javafx.application.Application;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCombination;
+import java.io.File;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -15,12 +19,11 @@ public class Shine extends Application {
         Menu toolsMenu = new Menu("Tools");
         Menu viewMenu = new Menu("View");
 
-        MenuItem midletItem = new MenuItem("Load Jar/Jad File");
+        MenuItem midletItem = new MenuItem("Load Jar");
         MenuItem recentItem = new MenuItem("Recent");
         MenuItem recordItem = new MenuItem("Record");
         MenuItem screenshotItem = new MenuItem("Screenshot");
         MenuItem exitItem = new MenuItem("Exit");
-        
         
         exitItem.setOnAction(e -> primaryStage.close());
 
@@ -29,12 +32,23 @@ public class Shine extends Application {
         menuBar.getMenus().addAll(midletMenu, toolsMenu, viewMenu);
 
         VBox root = new VBox(menuBar);
-        Scene scene = new Scene(root, 240, 320);
-
-        primaryStage.setTitle("Shine");
-        primaryStage.setScene(scene);
-        primaryStage.setResizable(false);
-        primaryStage.show();
+        Scene scene = new Scene(root, 160, 170);
+        
+    primaryStage.setTitle("Shine");
+    primaryStage.setScene(scene);
+    primaryStage.setResizable(false);
+    primaryStage.show();
+        
+ private void openJarFile(Stage stage) {
+     FileChooser fileChooser = new FileChooser();
+     fileChooser.setTitle(Load JAR File);
+     fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("JAR Files", "*.jar"));
+        
+     File selectedFile = fileChooser.showOpenDialog(stage);
+     if (selectedFile != null) {
+         System.out.println("Selected JAR: " + selectedFile.getAbsolutePath());
+     }
+ }
     }
     
     public static void main(String[] args) {
