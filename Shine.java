d69d6aaa0117b246fd3fae9fc80783b7892067c3
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-03-04 15:39:44 -0500

    Update Shine.java

--- Shine.java
+++ Shine.java
@@ -4,53 +4,57 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.BorderPane;
 import java.io.File;
-import javafx.scene.layout.VBox;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
 
 public class Shine extends Application {
+
     @Override
     public void start(Stage primaryStage) {
         
         MenuBar menuBar = new MenuBar();
 
-        Menu midletMenu = new Menu("Midlet");
-        Menu toolsMenu = new Menu("Tools");
-        Menu viewMenu = new Menu("View");
-
-        MenuItem midletItem = new MenuItem("Load Jar");
-        MenuItem recentItem = new MenuItem("Recent");
-        MenuItem recordItem = new MenuItem("Record");
-        MenuItem screenshotItem = new MenuItem("Screenshot");
+        Menu fileMenu = new Menu("Midlet");
+        MenuItem openItem = new MenuItem("Load JAR");
+        openItem.setAccelerator(KeyCombination.keyCombination("Ctrl+O"));
+        openItem.setOnAction(e -> openJarFile(primaryStage));
         MenuItem exitItem = new MenuItem("Exit");
-        
         exitItem.setOnAction(e -> primaryStage.close());
+        fileMenu.getItems().addAll(openItem, new SeparatorMenuItem(), exitItem);
 
-        midletMenu.getItems().addAll(midletItem, recentItem, recordItem, screenshotItem, new SeparatorMenuItem(), exitItem);
+        Menu viewMenu = new Menu("View");
+        MenuItem fullscreenItem = new MenuItem("Fullscreen");
+        fullscreenItem.setOnAction(e -> primaryStage.setFullScreen(!primaryStage.isFullScreen()));
+        viewMenu.getItems().add(fullscreenItem);
 
-        menuBar.getMenus().addAll(midletMenu, toolsMenu, viewMenu);
+        Menu toolsMenu = new Menu("Tools");
+        MenuItem settingsItem = new MenuItem("Settings");
+        toolsMenu.getItems().add(settingsItem);
 
-        VBox root = new VBox(menuBar);
+        menuBar.getMenus().addAll(fileMenu, viewMenu, toolsMenu);
+
+        BorderPane root = new BorderPane();
+        root.setTop(menuBar);
+
         Scene scene = new Scene(root, 160, 170);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Shine");
         
-    primaryStage.setTitle("Shine");
-    primaryStage.setScene(scene);
-    primaryStage.setResizable(false);
-    primaryStage.show();
-        
- private void openJarFile(Stage stage) {
-     FileChooser fileChooser = new FileChooser();
-     fileChooser.setTitle(Load JAR File);
-     fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("JAR Files", "*.jar"));
-        
-     File selectedFile = fileChooser.showOpenDialog(stage);
-     if (selectedFile != null) {
-         System.out.println("Selected JAR: " + selectedFile.getAbsolutePath());
-     }
- }
+        primaryStage.show();
     }
-    
+
+    private void openJarFile(Stage stage) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Load JAR File");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("JAR Files", "*.jar"));
+        
+        File selectedFile = fileChooser.showOpenDialog(stage);
+        if (selectedFile != null) {
+            System.out.println("Selected JAR: " + selectedFile.getAbsolutePath());
+
+        }
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
