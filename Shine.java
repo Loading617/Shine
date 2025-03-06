0b8aa8169bfade05d4493ceff4462d6a1d4bacdc
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-03-06 16:09:23 -0500

    Update Shine.java

--- Shine.java
+++ Shine.java
@@ -1,112 +1,63 @@
-import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.*;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.BorderPane;
 import java.io.File;
-import java.util.jar.JarFile;
-import java.util.jar.Manifest;
-import javax.microedition.lcdui.Graphics;
-import javax.microedition.lcdui.Canvas as J2MECanvas;
 
 public class Shine extends Application {
-
-    private File selectedJar;
-    private Label midletInfo;
-    private Canvas gameCanvas;
-    private GraphicsContext gc;
-    private boolean running = false;
 
     @Override
     public void start(Stage primaryStage) {
         
         MenuBar menuBar = new MenuBar();
 
-        Menu fileMenu = new Menu("File");
-        MenuItem openItem = new MenuItem("Open JAR...");
-        openItem.setOnAction(e -> openJarFile(primaryStage));
-        MenuItem runItem = new MenuItem("Run J2ME App");
-        runItem.setOnAction(e -> runJ2MEApp());
-        runItem.setDisable(true);
-        fileMenu.getItems().addAll(openItem, runItem);
+        Menu midletMenu = new Menu("Midlet");
+        MenuItem loadItem = new MenuItem("Load JAR");
+        MenuItem recentItem = new MenuItem("Recent");
+        MenuItem recordItem = new MenuItem("Record");
+        MenuItem screenshotItem = new  MenuItem("Screenshot");
+        loadItem.setAccelerator(KeyCombination.keyCombination("Ctrl+O"));
+        loadItem.setOnAction(e -> openJarFile(primaryStage));
+        MenuItem exitItem = new MenuItem("Exit");
+        exitItem.setAccelerator(KeyCombination.keyCombination("ESC"));
+        exitItem.setOnAction(e -> primaryStage.close());
+        midletMenu.getItems().addAll(loadItem, recentItem, recordItem, screenshotItem, new SeparatorMenuItem(), exitItem);
 
-        menuBar.getMenus().addAll(fileMenu);
+        Menu viewMenu = new Menu("View");
+        MenuItem fullscreenItem = new MenuItem("Fullscreen");
+        fullscreenItem.setOnAction(e -> primaryStage.setFullScreen(!primaryStage.isFullScreen()));
+        viewMenu.getItems().add(fullscreenItem);
 
-        midletInfo = new Label("No J2ME app loaded");
+        Menu toolsMenu = new Menu("Tools");
+        MenuItem settingsItem = new MenuItem("Preferences");
+        toolsMenu.getItems().add(settingsItem);
 
-        gameCanvas = new Canvas(240, 320);
-        gc = gameCanvas.getGraphicsContext2D();
+        menuBar.getMenus().addAll(midletMenu, viewMenu, toolsMenu);
 
         BorderPane root = new BorderPane();
         root.setTop(menuBar);
-        root.setCenter(gameCanvas);
-        root.setBottom(midletInfo);
 
-        Scene scene = new Scene(root, 800, 600);
+        Scene scene = new Scene(root, 160, 170);
         primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.setTitle("Shine");
-
+        
         primaryStage.show();
     }
 
     private void openJarFile(Stage stage) {
         FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Open JAR File");
+        fileChooser.setTitle("Load JAR File");
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("JAR Files", "*.jar"));
-
-        File file = fileChooser.showOpenDialog(stage);
-        if (file != null) {
-            selectedJar = file;
-            extractMIDletInfo(selectedJar);
-        }
-    }
-
-    private void extractMIDletInfo(File jarFile) {
-        try (JarFile jar = new JarFile(jarFile)) {
-            Manifest manifest = jar.getManifest();
-            String name = manifest.getMainAttributes().getValue("MIDlet-Name");
-            String version = manifest.getMainAttributes().getValue("MIDlet-Version");
-            midletInfo.setText("Loaded: " + name + " (v" + version + ")");
-        } catch (Exception e) {
-            midletInfo.setText("Error loading MIDlet info");
-            e.printStackTrace();
-        }
-    }
-
-    private void runJ2MEApp() {
-        if (selectedJar == null) {
-            midletInfo.setText("No JAR file selected!");
-            return;
-        }
-
-        System.out.println("Running J2ME App: " + selectedJar.getAbsolutePath());
-        running = true;
-
-        new AnimationTimer() {
-            @Override
-            public void handle(long now) {
-                if (running) {
-                    updateGame();
-                    renderGame();
-                }
-            }
-        }.start();
-    }
-
-    private void updateGame() {
         
-    }
+        File selectedFile = fileChooser.showOpenDialog(stage);
+        if (selectedFile != null) {
+            System.out.println("Selected JAR: " + selectedFile.getAbsolutePath());
 
-    private void renderGame() {
-        gc.clearRect(0, 0, gameCanvas.getWidth(), gameCanvas.getHeight());
-        
-        gc.strokeRect(50, 50, 100, 100);
-        gc.fillText("J2ME Game Running!", 70, 200);
+        }
     }
 
     public static void main(String[] args) {
