11a58919a963402870b71aa840ef6699bfb3fb34
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-03-09 18:18:15 -0400

    Update Shine.java

--- Shine.java
+++ Shine.java
@@ -1,26 +1,47 @@
 import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.image.WritableImage;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javax.imageio.ImageIO;
 import java.io.File;
+import java.io.IOException;
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import javax.sound.sampled.*;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
 
 public class Shine extends Application {
+    private boolean recording = false;
+    private ByteArrayOutputStream audioOut;
+    private TargetDataLine line;
 
     @Override
     public void start(Stage primaryStage) {
-        
         MenuBar menuBar = new MenuBar();
 
         Menu midletMenu = new Menu("Midlet");
         MenuItem loadItem = new MenuItem("Load JAR");
         MenuItem recentItem = new MenuItem("Recent");
         MenuItem recordItem = new MenuItem("Record");
-        MenuItem screenshotItem = new  MenuItem("Screenshot");
+        MenuItem screenshotItem = new MenuItem("Screenshot");
         loadItem.setAccelerator(KeyCombination.keyCombination("Ctrl+O"));
         loadItem.setOnAction(e -> openJarFile(primaryStage));
+        
+        recordItem.setOnAction(e -> toggleRecording());
+        screenshotItem.setOnAction(e -> takeScreenshot(primaryStage));
+
         MenuItem exitItem = new MenuItem("Exit");
         exitItem.setAccelerator(KeyCombination.keyCombination("ESC"));
         exitItem.setOnAction(e -> primaryStage.close());
@@ -44,7 +65,6 @@ public class Shine extends Application {
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.setTitle("Shine");
-        
         primaryStage.show();
     }
 
@@ -56,7 +76,81 @@ public class Shine extends Application {
         File selectedFile = fileChooser.showOpenDialog(stage);
         if (selectedFile != null) {
             System.out.println("Selected JAR: " + selectedFile.getAbsolutePath());
+        }
+    }
 
+    private void takeScreenshot(Stage stage) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save Screenshot");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PNG Files", "*.png"));
+        File file = fileChooser.showSaveDialog(stage);
+        
+        if (file != null) {
+            try {
+                Robot robot = new Robot();
+                Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+                BufferedImage capture = robot.createScreenCapture(screenRect);
+                ImageIO.write(capture, "png", file);
+                System.out.println("Screenshot saved: " + file.getAbsolutePath());
+            } catch (AWTException | IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    private void toggleRecording() {
+        if (!recording) {
+            startRecording();
+        } else {
+            stopRecording();
+        }
+    }
+
+    private void startRecording() {
+        new Thread(() -> {
+            try {
+                AudioFormat format = new AudioFormat(44100, 16, 2, true, true);
+                DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+                if (!AudioSystem.isLineSupported(info)) {
+                    System.out.println("Line not supported");
+                    return;
+                }
+                line = (TargetDataLine) AudioSystem.getLine(info);
+                line.open(format);
+                line.start();
+                
+                audioOut = new ByteArrayOutputStream();
+                byte[] buffer = new byte[4096];
+                recording = true;
+                System.out.println("Recording started...");
+                
+                while (recording) {
+                    int bytesRead = line.read(buffer, 0, buffer.length);
+                    audioOut.write(buffer, 0, bytesRead);
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }).start();
+    }
+
+    private void stopRecording() {
+        recording = false;
+        line.stop();
+        line.close();
+        
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save Recording");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("WAV Files", "*.wav"));
+        File file = fileChooser.showSaveDialog(null);
+        
+        if (file != null) {
+            try (FileOutputStream fos = new FileOutputStream(file)) {
+                fos.write(audioOut.toByteArray());
+                System.out.println("Recording saved: " + file.getAbsolutePath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
