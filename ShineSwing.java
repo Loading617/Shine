86d8a41668ed69c3e622b5ccad38a43be9623211
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-04-12 17:08:44 -0400

    Update ShineSwing.java

--- ShineSwing.java
+++ ShineSwing.java
@@ -29,40 +29,69 @@ public class ShineSwing {
         JMenuItem recordItem = new JMenuItem("Record");
         JMenuItem screenshotItem = new JMenuItem("Screenshot");
         JMenuItem exitItem = new JMenuItem("Exit");
-
+    
         loadItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_DOWN_MASK));
         exitItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0));
-
+    
         loadItem.addActionListener(e -> openJarFile());
         recordItem.addActionListener(e -> toggleRecording(frame));
         screenshotItem.addActionListener(e -> takeScreenshot(frame));
         exitItem.addActionListener(e -> frame.dispose());
-
+    
         midletMenu.add(loadItem);
         midletMenu.add(recentItem);
         midletMenu.add(recordItem);
         midletMenu.add(screenshotItem);
         midletMenu.addSeparator();
         midletMenu.add(exitItem);
-
+    
         JMenu viewMenu = new JMenu("View");
-        JMenuItem fullscreenItem = new JMenuItem("Fullscreen");
-        fullscreenItem.addActionListener(e -> {
-            frame.dispose();
-            frame.setUndecorated(!frame.isUndecorated());
-            frame.setVisible(true);
-            frame.setExtendedState(frame.getExtendedState() ^ JFrame.MAXIMIZED_BOTH);
-        });
-        viewMenu.add(fullscreenItem);
 
+        JMenu screenSizeMenu = new JMenu("Screen Size");
+        screenSizeMenu.add(new JMenuItem("128x160"));
+        screenSizeMenu.add(new JMenuItem("240x320"));
+        screenSizeMenu.add(new JMenuItem("360x640"));
+        screenSizeMenu.add(new JMenuItem("Custom…"));
+
+        JMenu orientationMenu = new JMenu("Orientation");
+        orientationMenu.add(new JMenuItem("Portrait"));
+        orientationMenu.add(new JMenuItem("Landscape"));
+
+        JMenu zoomMenu = new JMenu("Zoom");
+        zoomMenu.add(new JMenuItem("1x"));
+        zoomMenu.add(new JMenuItem("2x"));
+        zoomMenu.add(new JMenuItem("Fit to Window"));
+
+        JCheckBoxMenuItem showKeypad = new JCheckBoxMenuItem("Show Keypad", true);
+        JCheckBoxMenuItem showSkin = new JCheckBoxMenuItem("Show Phone Skin", false);
+
+        JMenu themeMenu = new JMenu("Theme");
+        themeMenu.add(new JMenuItem("Light"));
+        themeMenu.add(new JMenuItem("Dark"));
+
+        JCheckBoxMenuItem showFPS = new JCheckBoxMenuItem("Show FPS");
+        JCheckBoxMenuItem showDebugInfo = new JCheckBoxMenuItem("Show Debug Info");
+
+        viewMenu.add(screenSizeMenu);
+        viewMenu.add(orientationMenu);
+        viewMenu.add(zoomMenu);
+        viewMenu.addSeparator();
+        viewMenu.add(showKeypad);
+        viewMenu.add(showSkin);
+        viewMenu.addSeparator();
+        viewMenu.add(themeMenu);
+        viewMenu.addSeparator();
+        viewMenu.add(showFPS);
+        viewMenu.add(showDebugInfo);
+    
         JMenu toolsMenu = new JMenu("Tools");
         JMenuItem settingsItem = new JMenuItem("Preferences");
         toolsMenu.add(settingsItem);
-
+    
         menuBar.add(midletMenu);
         menuBar.add(viewMenu);
         menuBar.add(toolsMenu);
-
+    
         frame.setJMenuBar(menuBar);
         frame.setVisible(true);
     }
